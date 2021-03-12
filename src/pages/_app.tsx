5cea623c8fe3f408638f176dba599188ff6450ff
danmaq <info@danmaq.com>
2021-03-12 06:45:19 +0000

    feat: added the tailwindcss

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from 'next/app';
 import type { FC } from 'react';
+import 'tailwindcss/tailwind.css';
 
 const App: FC<AppProps> = ({ Component, pageProps }) => (
   // eslint-disable-next-line react/jsx-props-no-spreading
