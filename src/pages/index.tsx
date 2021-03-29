60c74c20de15d1985bb27e994fd1df8a86ec1a74
danmaq <info@danmaq.com>
2021-03-29 07:00:11 +0000

    feat: linked the some components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,28 @@
 import Head from 'next/head';
 import type { FC } from 'react';
+import Footer from '../components/atoms/Footer';
+import GuestHeader from '../components/atoms/GuestHeader';
+import Image from '../components/atoms/Image';
+import LoginForm from '../components/molecules/LoginForm';
+import LoginIssues from '../components/molecules/LoginIssues';
+import type { WithoutChildren } from '../components/types';
 
-const Component: FC = () => (
-  <div>
+const Component: FC<WithoutChildren> = () => (
+  <>
     <Head>
       <title>Hello, world!</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
     </Head>
 
-    <main>
-      <h1>Hello, world!</h1>
+    <GuestHeader />
+    <main className="mx-auto">
+      <LoginForm />
+      <LoginIssues />
+      <Image width={527} height={138} />
     </main>
-  </div>
+    <Footer />
+  </>
 );
 Component.displayName = 'Index';
 
