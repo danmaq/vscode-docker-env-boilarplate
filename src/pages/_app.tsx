6285d567d746e17e6571dfd56c24be11f02cdc9d
danmaq <info@danmaq.com>
2021-03-29 06:58:11 +0000

    feat: added an empty store

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,14 @@
 import type { AppProps } from 'next/app';
 import type { FC } from 'react';
+import { Provider } from 'react-redux';
 import 'tailwindcss/tailwind.css';
+import store from '../store';
 
 const App: FC<AppProps> = ({ Component, pageProps }) => (
-  // eslint-disable-next-line react/jsx-props-no-spreading
-  <Component {...pageProps} />
+  <Provider store={store}>
+    {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+    <Component {...pageProps} />
+  </Provider>
 );
 App.displayName = 'App';
 
